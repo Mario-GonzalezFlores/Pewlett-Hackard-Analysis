943b175f736793a79201714e03928be875280729
Mario Alberto González Flores <89816213+Mario-GonzalezFlores@users.noreply.github.com>
2021-11-01 18:42:59 -0600

    Complete Module

--- schema.sql
+++ schema.sql
@@ -202,3 +202,29 @@ ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
 
+
+-- Sales team information
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name	
+--INTO sales_info
+FROM current_emp as ce
+INNER JOIN dept_employees AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name LIKE 'Sales';
+
+-- Sales and Development teams information
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name	
+INTO sales_dev_info
+FROM current_emp as ce
+INNER JOIN dept_employees AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development');
