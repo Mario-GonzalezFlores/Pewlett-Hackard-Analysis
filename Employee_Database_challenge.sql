84a4b4bd3b177e23fe7a7ffb3b059bcfb7643ff4
Mario Alberto González Flores <89816213+Mario-GonzalezFlores@users.noreply.github.com>
2021-11-03 20:51:28 -0600

    Complete Deliverable 2

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Create table of retiring employees by title
 SELECT e.emp_no,
     e.first_name,
 	e.last_name,
@@ -11,9 +12,7 @@ ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no;
 
-SELeCT * FROM retirement_titles;
-DROP TABLE retirement_titles;
-
+-- Unique retiring employees by title
 SELECT DISTINCT ON (emp_no)
 emp_no,
 first_name,
@@ -26,3 +25,29 @@ ORDER BY emp_no asc, to_date desc;
 
 SELeCT * FROM unique_titles;
 DROP TABLE unique_titles;
+
+-- Retiring personel by title (gruop by)
+SELECT COUNT (emp_no) as count,
+title
+
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+-- Mentorship elegibility
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	de.from_date,	
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_employees AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (t.to_date = '9999-01-01')
+ORDER BY emp_no ASC;
